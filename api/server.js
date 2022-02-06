afbb5c0bb3623387ec85b956b775b2f652913e5b
Matt <86365526+matthewstack@users.noreply.github.com>
2022-02-06 18:06:46 -0500

    delete method

--- api/server.js
+++ api/server.js
@@ -63,4 +63,21 @@ server.get("/api/users/:id", async (req, res) => {
   }
 });
 
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.remove(req.params.id);
+    if (!deletedUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      res.json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user could not be removed",
+    });
+  }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
