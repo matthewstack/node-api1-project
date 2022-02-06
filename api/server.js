02a9cd98bcb1c5cfbf40cdca1a46d2e4b39e3686
Matt <86365526+matthewstack@users.noreply.github.com>
2022-02-06 18:15:44 -0500

    finished mvp

--- api/server.js
+++ api/server.js
@@ -80,4 +80,29 @@ server.delete("/api/users/:id", async (req, res) => {
   }
 });
 
+server.put("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  try {
+    if (!name || !bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user",
+      });
+    } else {
+      const updatedUser = await User.update(id, { name, bio });
+      if (!updatedUser) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      } else {
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    });
+  }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
